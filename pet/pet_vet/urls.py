168c398831f0a4a61172fd4b18fefce270707b85
Ramazan <kworn76@yandex.kz>
2023-03-28 17:14:28 +0600

    добавляю услуги

--- pet/pet_vet/urls.py
+++ pet/pet_vet/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, services, shop, contacts, about
 
 urlpatterns = [
     path('', index, name='index'),
     path('vet/', index, name='index'),
+    path('services/', services, name='services'),
+    path('shop/', shop, name='shop'),
+    path('contacts/', contacts, name='contacts'),
+    path('about/', about, name='about'),
 ]
--- pet/pet_vet/views.py
+++ pet/pet_vet/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 
 def index(request):
     return render(request, 'pet_vet/index.html')
+
+
+def services(request):
+    return render(request, 'pet_vet/services.html')
+
+
+def shop(request):
+    return render(request, 'pet_vet/shop.html')
+
+
+def contacts(request):
+    return render(request, 'pet_vet/contacts.html')
+
+
+def about(request):
+    return render(request, 'pet_vet/about.html')
